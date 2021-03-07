d925d51c6456eac6cc24107a33f0d58ae41e88bd
cesaremalosso <cesare.malosso@gmail.com>
2021-03-07 11:52:46 +0100

    added gk to cepstral_analysis.py

--- cepstral_analysis.py
+++ cepstral_analysis.py
@@ -1,35 +1,62 @@
 #!/usr/bin/python
 
 import sys
-sys.path.append('/marconi/home/userexternal/cmalosso/thermocepstrum/themocepstrum')
+#sys.path.append('/marconi/home/userexternal/cmalosso/thermocepstrum/themocepstrum')
 import thermocepstrum as tc
 import numpy as np
 import argparse
+from scipy import signal
 
+def autocorr(x):
+    result = signal.correlate(x, x, mode='full', method='fft')
+    v = [result[i]/( len(x)-abs( i - (len(x))  ) ) for i in range(len(result))]
+    return v[int(result.size/2):]
 
-# data in input = la temperatura
+def green_kubo(data, tmax, dt_fs):
+    jen = data[:, 3:]
+    temp = data[:, 2]
+
+    Nstep = int(np.rint(tmax / (dt_fs * 1e-3)))
+    maxrows = np.size(jen, 0)
+    Ncurrs = maxrows // Nstep
+
+    result = np.zeros((Ncurrs, 2))
+    for ij in range(Ncurrs):
+        init = Nstep * ij
+        end = Nstep * (ij + 1) if Nstep * (ij + 1) < jen.shape[0] else jen.shape[0]
+
+        tmean = np.mean(temp[init:end])
+
+        corr = []
+        for i in range(3):
+            corr.append(np.array(autocorr(jen[init:end, i])))
+        mean_corr = (corr[0]+corr[1]+corr[2])/3
+        result[ij] = [tmean, np.sum(mean_corr)]
+    return result
+
 def block_analysis(data, tmax, dt_fs, vol):
     jen = data[:,3:]
     temp = data[:,2]
 
     Nstep = int(np.rint(tmax / (dt_fs * 1e-3)))
     maxrows = np.size(jen, 0)
-    Ncurrs = maxrows / Nstep
+    Ncurrs = maxrows // Nstep
 
     psds1 = 0
     psds2 = 0
 
-    T = []
+    result = np.zeros([Ncurrs,2])
 
-    for ij in range(int(Ncurrs)):
+    for ij in range(Ncurrs):
         init = Nstep * ij
         end = Nstep * (ij + 1) if Nstep * (ij + 1) < jen.shape[0] else jen.shape[0]
 
         Tmean = np.mean(temp[init:end])
-        T.append(Tmean)
 
         jj = tc.HeatCurrent(j=jen[init:end], DT_FS=dt_fs, TEMPERATURE=Tmean, units='metal_vis', VOLUME=vol,
                             PSD_FILTER_W=0.05)
+
+        result[ij] = [Tmean, jj.psd[0]*jj.kappa_scale/2]
 
         # S(w=0) average
         psds1 += (jj.psd[0] - psds1) / (ij + 1)
@@ -37,8 +64,7 @@ def block_analysis(data, tmax, dt_fs, vol):
 
     psdstd = np.sqrt((psds2 - psds1 ** 2) / (Ncurrs - 1))
 
-    result = [T, psds1 * jj.kappa_scale / 2, psdstd * jj.kappa_scale / 2]
-    return result
+    return result, psds1 * jj.kappa_scale / 2, psdstd * jj.kappa_scale / 2
 
 def main():
     parser = argparse.ArgumentParser(description='Block analysis script')
@@ -75,8 +101,20 @@ def main():
 
 
     for time in args.timelist:
-        block_result = block_analysis(data, time, args.dt, args.vol)
-        np.savetxt('{}ps_result.out'.format(time), block_result, fmt='%d')
+        block_result, mean, mean_std = block_analysis(data, time, args.dt, args.vol)
+        print(mean, mean_std)
+        np.savetxt('{}ps_result.out'.format(time), block_result)
+
+    kB = 1.3806504
+    bartoPa = 1
+    for time in args.timelist:
+
+        gk_result = green_kubo(data, time, args.dt)
+        for i in range(np.size(gk_result,0)):
+            conv = args.vol / kB / gk_result[i,0] * bartoPa ** 2 * 1e-11
+            gk_result[i,1] *= conv
+        np.savetxt('{}ps_gk-result.out'.format(time), gk_result)
+
 
 if __name__ == "__main__":
     main()
