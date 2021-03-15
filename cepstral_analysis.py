1f05045da740897471bf192df23636a791c445e4
cesaremalosso <cesare.malosso@gmail.com>
2021-03-15 09:02:36 +0100

    modified cepstral_analysis.py

--- cepstral_analysis.py
+++ cepstral_analysis.py
@@ -12,15 +12,13 @@ def autocorr(x):
     v = [result[i]/( len(x)-abs( i - (len(x))  ) ) for i in range(len(result))]
     return v[int(result.size/2):]
 
-def green_kubo(data, tmax, dt_fs):
-    jen = data[:, 3:]
-    temp = data[:, 2]
+def green_kubo(jen, temp, tmax, gk_tmax, dt_fs):
 
     Nstep = int(np.rint(tmax / (dt_fs * 1e-3)))
     maxrows = np.size(jen, 0)
     Ncurrs = maxrows // Nstep
 
-    result = np.zeros((Ncurrs, 2))
+    result = np.zeros((Ncurrs, 11))
     for ij in range(Ncurrs):
         init = Nstep * ij
         end = Nstep * (ij + 1) if Nstep * (ij + 1) < jen.shape[0] else jen.shape[0]
@@ -32,13 +30,13 @@ def green_kubo(data, tmax, dt_fs):
             corr.append(np.array(autocorr(jen[init:end, i])))
         mean_corr = (corr[0]+corr[1]+corr[2])/3
         np.savetxt('{}_auto.out'.format(ij), mean_corr)
-        result[ij] = [tmean, np.sum(mean_corr[:np.size(mean_corr)//4])]
+        result[ij] = [tmean,np.sum(mean_corr[:int(50e3)]),np.sum(mean_corr[:int(100e3)]), np.sum(mean_corr[:int(150e3)]),
+                      np.sum(mean_corr[:int(200e3)]), np.sum(mean_corr[:int(250e3)]), np.sum(mean_corr[:int(300e3)]),
+                      np.sum(mean_corr[:int(350e3)]), np.sum(mean_corr[:int(400e3)]),np.sum(mean_corr[:int(450e3)]), np.sum(mean_corr[:int(500e3)])]
 
     return result
 
-def block_analysis(data, tmax, dt_fs, vol):
-    jen = data[:, 3:]
-    temp = data[:, 2]
+def block_analysis(jen, temp, tmax, dt_fs, vol):
 
     Nstep = int(np.rint(tmax / (dt_fs * 1e-3)))
     maxrows = np.size(jen, 0)
@@ -87,9 +85,17 @@ def main():
                         type=int,
                         required=True,
                         help='last file')
+    parser.add_argument('--tmax',
+                        type = int,
+                        required=True,
+                        help='green-kubo extremum of integration in ps')
     parser.add_argument('--timelist',
                         type = int,
                         nargs='+',
+                        required=True,
+                        help='list of the size of blocks for block-analysis')
+    parser.add_argument('-u', '--units',
+                        type = str,
                         required=True,
                         help='list of the size of blocks for block-analysis')
     args = parser.parse_args()
@@ -97,6 +103,7 @@ def main():
     first = args.first
     last = args.last
     vol = args.box**3
+    units = args.units
 
     lista = []
     for i in range(first,last+1):
@@ -106,21 +113,35 @@ def main():
             lista.append(np.loadtxt('stress{}.{:.1f}fs.out'.format(i, args.dt), skiprows=1))
     data = np.concatenate(lista)
 
+    jen = data[:,3:6]
+    temp = data [:,2]
 
-    for time in args.timelist:
-        block_result, mean, mean_std = block_analysis(data, time, args.dt, vol)
-        print(mean, mean_std)
-        np.savetxt('{}ps_result.out'.format(time), block_result)
+    if units == 'metal':
+        print('metal units')
+    elif units == 'real':
+        jen *= 1.01325
+        print('real units')
+
+#    for time in args.timelist:
+#        block_result, mean, mean_std = block_analysis(jen, temp, time, args.dt, vol)
+#        print(mean, mean_std)
+#        np.savetxt('{}ps_result.out'.format(time), block_result)
 
     kB = 1.3806504
     bartoPa = 1
     for time in args.timelist:
-
-        gk_result = green_kubo(data, time, args.dt)
+        gk_result = green_kubo(jen, temp, time, args.tmax, args.dt)
         for i in range(np.size(gk_result,0)):
             conv = vol / kB / gk_result[i,0] * bartoPa ** 2 * 1e-11
-            gk_result[i,1] *= conv
-        np.savetxt('{}ps_gk-result.out'.format(time), gk_result)
+            gk_result[i,1:] *= conv
+        with open('{}ps_gk-result.out'.format(time),'w') as f:
+#            f.write('# mean temperature \t tmax = {} \t tmax = {} \t tmax = {}\n'.format(args.tmax/2,
+#                                                                                       args.tmax, args.tmax*2))
+            np.savetxt(f, gk_result)
+            mean = np.array([np.mean(gk_result[:,i]) for i in range(11)])
+            f.write('# mean values \n')
+            np.savetxt(f, mean.reshape(1, mean.shape[0]))
+#            f.write('{} \t {} \t {} \t {}'.format(np.mean(gk_result[:,0]), np.mean(gk_result[:,1]), np.mean(gk_result[:,2]), np.mean(gk_result[:,3])))
 
 
 if __name__ == "__main__":
