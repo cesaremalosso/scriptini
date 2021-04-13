89203431a6745e4ef0b74dfb219a527e8c41de48
cesaremalosso <cesare.malosso@gmail.com>
2021-04-13 17:04:58 +0200

    modified some stuff

--- gk_integration.py
+++ gk_integration.py
@@ -18,21 +18,20 @@ def green_kubo(jen, temp, tmax, gk_tmax, dt_fs):
     maxrows = np.size(jen, 0)
     Ncurrs = maxrows // Nstep
 
-    result = np.zeros((Ncurrs, 12))
+    result = np.zeros((Ncurrs, 8))
     for ij in range(Ncurrs):
         init = Nstep * ij
         end = Nstep * (ij + 1) if Nstep * (ij + 1) < jen.shape[0] else jen.shape[0]
 
-        tmean = np.mean(temp[init:end])
+        tmean = np.mean(temp[init:end])*191/192
 
         corr = []
         for i in range(3):
             corr.append(np.array(autocorr(jen[init:end, i])))
         mean_corr = (corr[0]+corr[1]+corr[2])/3
         np.savetxt('{}_auto.out'.format(ij), mean_corr)
-        result[ij] = [tmean, np.sum(mean_corr[:int(200e3)]), np.sum(mean_corr[:int(250e3)]), np.sum(mean_corr[:int(300e3)]),
-                      np.sum(mean_corr[:int(350e3)]), np.sum(mean_corr[:int(400e3)]), np.sum(mean_corr[:int(450e3)]), np.sum(mean_corr[:int(500e3)]),
-                      np.sum(mean_corr[:int(750e3)]), np.sum(mean_corr[:int(1000e3)]), np.sum(mean_corr[:int(2000e3)]), np.sum(mean_corr[:int(4000e3)])]
+        result[ij] = [tmean, np.sum(mean_corr[:int(25e3)]), np.sum(mean_corr[:int(50e3)]), np.sum(mean_corr[:int(75e3)]),
+                      np.sum(mean_corr[:int(100e3)]), np.sum(mean_corr[:int(200e3)]), np.sum(mean_corr[:int(300e3)]), np.sum(mean_corr[:int(400e3)])]
 
     return result
 
@@ -45,22 +44,25 @@ def block_analysis(jen, temp, tmax, dt_fs, vol):
     psds1 = 0
     psds2 = 0
 
-    result = np.zeros([Ncurrs,2])
+    result = np.zeros([Ncurrs,3])
 
     for ij in range(Ncurrs):
         init = Nstep * ij
         end = Nstep * (ij + 1) if Nstep * (ij + 1) < jen.shape[0] else jen.shape[0]
 
-        Tmean = np.mean(temp[init:end])
+        Tmean = np.mean(temp[init:end])*191/192
 
         jj = tc.HeatCurrent(j=jen[init:end], DT_FS=dt_fs, TEMPERATURE=Tmean, units='metal_vis', VOLUME=vol,
                             PSD_FILTER_W=0.05)
 
-        result[ij] = [Tmean, jj.psd[0]*jj.kappa_scale/2]
+        rj = jj.resample_current(fstar_THz=4, plot=False, PSD_FILTER_W=0.10)
+        rj.cepstral_analysis(Kmin_corrfactor=2)
+
+        result[ij] = [Tmean, rj.dct.psd[0]*jj.kappa_scale/2, rj.kappa_Kmin_std]
 
         # S(w=0) average
-        psds1 += (jj.psd[0] - psds1) / (ij + 1)
-        psds2 += (jj.psd[0] ** 2 - psds2) / (ij + 1)
+        psds1 += (rj.dct.psd[0] - psds1) / (ij + 1)
+        psds2 += (rj.dct.psd[0] ** 2 - psds2) / (ij + 1)
     if Ncurrs > 1:
         psdstd = np.sqrt((psds2 - psds1 ** 2) / (Ncurrs - 1))
     else:
@@ -97,7 +99,7 @@ def main():
     parser.add_argument('-u', '--units',
                         type = str,
                         required=True,
-                        help='list of the size of blocks for block-analysis')
+                        help='units real or metal supported')
     args = parser.parse_args()
 
     first = args.first
@@ -122,23 +124,27 @@ def main():
         jen *= 1.01325
         print('real units')
 
-#    for time in args.timelist:
-#        block_result, mean, mean_std = block_analysis(jen, temp, time, args.dt, vol)
-#        print(mean, mean_std)
-#        np.savetxt('{}ps_result.out'.format(time), block_result)
+    for time in [100,200,500]:
+        block_result, mean, mean_std = block_analysis(jen, temp, time, args.dt, vol)
+        print(mean, mean_std)
+        with open('{}ps_result.out'.format(time),'w') as f:
+            np.savetxt(f, block_result)
+            f.write('\t \t {} \n'.format(np.mean(block_result[:,2])))
+            f.write('# mean values \n')
+            f.write('{} \t {} \t {}'.format(np.mean(block_result[:,0]),mean,mean_std))
 
     kB = 1.3806504
     bartoPa = 1
     for time in args.timelist:
         gk_result = green_kubo(jen, temp, time, args.tmax, args.dt)
         for i in range(np.size(gk_result,0)):
-            conv = vol / kB / gk_result[i,0] * bartoPa ** 2 * 1e-11
+            conv = vol / kB / gk_result[i,0] * args.dt * bartoPa ** 2 * 1e-11
             gk_result[i,1:] *= conv
         with open('{}ps_gk-result.out'.format(time),'w') as f:
 #            f.write('# mean temperature \t tmax = {} \t tmax = {} \t tmax = {}\n'.format(args.tmax/2,
 #                                                                                       args.tmax, args.tmax*2))
             np.savetxt(f, gk_result)
-            mean = np.array([np.mean(gk_result[:,i]) for i in range(12)])
+            mean = np.array([np.mean(gk_result[:,i]) for i in range(8)])
             f.write('# mean values \n')
             np.savetxt(f, mean.reshape(1, mean.shape[0]))
 #            f.write('{} \t {} \t {} \t {}'.format(np.mean(gk_result[:,0]), np.mean(gk_result[:,1]), np.mean(gk_result[:,2]), np.mean(gk_result[:,3])))
