fc8511bd25a0eb1b4a1d93da482091bca0f29570
Cesare Malosso <cmalosso@paperone.phys.sissa.it>
2021-10-05 18:26:24 +0200

    .

---
+++ m100/NVE_generator.py
@@ -0,0 +1,12 @@
+import numpy as np
+import argparse
+
+def write_nve(outf):
+    with open(outf, 'w') as of :
+        of.write()
+
+    return
+def write_nvt():
+    return
+if(__name__ == "main"):
+    parser = argparse.ArgumentParser(description='Generate NVE input files')
