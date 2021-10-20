5c3c99bcf1d8a0add8fe35124013799059538119
cesaremalosso <cesare.malosso@gmail.com>
2021-10-20 17:29:43 +0200

    added msd to pp_analysis

--- mymodules/pp_analysis.py
+++ mymodules/pp_analysis.py
@@ -17,7 +17,7 @@ def read_gdr(filein,output,nmol,vol,dr):
     return gdr_n
 
 def plot_gdr(gdr):
-    f,ax = plt.subplots(1, figsize=(6.0,4.0), constrained_layout=True)
+    f,ax = plt.subplots(1, figsize=(8.0,6.0), constrained_layout=True)
     ls=12
     #
     ax.tick_params(axis='x', labelsize=ls)
@@ -40,7 +40,30 @@ def read_vdos(filein,output,dt,nstep):
     vdos_n[:,3] = conv * (vdos[:,7] + vdos[:,9] + vdos[:,11])
     vdos_n[:,4] = np.sqrt( conv ** 2 * (vdos[:,8] + vdos[:,10] + vdos[:,12]) )
 
-    with open(output,'w') as fo:
+    with open(output, 'w') as fo:
         np.savetxt(output,vdos_n, fmt = '%.18e', header = 'freq [THz] \t VDOS_1 \t sigma_1 \t VDOS_2 \t  sigma_2')
 
     return vdos_n
+
+def read_msd(filein,output,dt):
+    msd = np.loadtxt(filein, skiprows=7)
+    time = [i*dt for i in range(np.shape(msd)[0])]
+    msd_n = np.transpose( np.vstack( (time, msd[:,0], np.sqrt(msd[:,1]), msd[:,2], np.sqrt(msd[:,3]),
+                                      msd[:,4], np.sqrt(msd[:,5]), msd[:,6], np.sqrt(msd[:,7])) ) )
+    with open(output, 'w') as fo:
+        np.savetxt(output, msd_n, fmt = '%.18e', header = 'time [ps] \t msd_1 \t sigma_1 \t msd_2 \t sigma_2 \t msd_cm1'
+                                                          ' \t sigma_cm1 \t sigma_cm2 \t msd_cm2 ')
+    return msd_n
+
+def plot_msd(msd):
+    f,ax = plt.subplots(1, figsize=(8.0,6.0), constrained_layout=True)
+    ls=12
+    #
+    ax.tick_params(axis='x', labelsize=ls)
+    ax.tick_params(axis='y', labelsize=ls)
+    col = ['red', 'blue']
+    lab = ['msd_1', 'msd_2']
+    for i in range(2):
+        ax.fill_between(msd[:,0],msd[:,2*i+1]-msd[:,2*i+2],msd[:,2*i+1]+msd[:,2*i+2], color = col[i])
+        ax.plot(msd[:,0],msd[:,2*i+1], label = lab[i], color = col[i])
+    plt.legend()
