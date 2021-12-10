6ecd5858145fd0d8119a8724fc29a6e9e1f8fb4b
cesaremalosso <cmalosso@sissa.it>
2021-12-10 15:55:17 +0100

    Create stress2virial.py

---
+++ stress2virial.py
@@ -0,0 +1,35 @@
+#!/bin/env python
+##### coding=utf-8
+
+import numpy as np
+import argparse
+mass = {}
+mass[1] = 1
+mass[2] = 16
+vol = 12.425508894894122**3
+natom = 192
+eV = 6.24150974#e18 #J2eV
+NA = 6.02214076#e23
+out_virial_raw = 'virial.raw'
+with open('stress.raw','r') as filestr, open('vel.raw', 'r') as filevel, open('type.raw', 'r') as filetyp:
+
+    linestr = filestr.readline()
+    # convert from bar to eV
+    values = np.array(linestr.split()) * vol * eV * 1e-7
+    stress = np.reshape(values, (3, 3))
+
+    linevel = filevel.readline()
+    values = np.array(linevel.split())
+    vel = np.reshape(values, (natom, 3))
+
+    linetyp = filetyp.readline()
+    typ = np.array(linetyp.split(), fmt=int)
+
+    kin = np.zeros((3,3))
+    for tatom, iatom in enumerate(typ):
+        kin += np.outer(vel[iatom], vel[iatom]) * mass[tatom] / NA * eV * 1e-4
+
+    virial = stress - kin
+
+    np.savetxt(out_virial_raw, np.reshape(virial, 9), newline=" ")
+    out_virial_raw.write('\n'.encode("utf-8"))
