497d5c2598cc9c12ab19cd83cf84f957155c570c
cesaremalosso <cmalosso@sissa.it>
2023-05-18 15:11:30 +0200

    added numba acceleration

--- mymodules/order_parameters.py
+++ mymodules/order_parameters.py
@@ -2,57 +2,92 @@ import numpy as np
 import scipy
 from scipy.constants import k as kb
 from scipy.constants import N_A
+from numba import jit, prange
 
-class Descriptor:
+@jit(nopython=True, parallel=True)
+def compute_q(coord, box, iatom):
 
-    def __init__(self, coord, box, type_list = None):
-        self.coord = coord
-        self.box = box
-        self.type_list = type_list
+    dist = coord - coord[iatom]
+    dist = apply_pbc(box, dist)
+    # array with the distances of the neighbours in order and list of neighbours in order
+    nn_dist, nn_list = compute_nn(dist)
+    # array with all the relative positions of the neighbours with respect to atom iatom
+    nn_matrixdiff = dist[nn_list]
 
-    def compute_descr(self, iatom, itype = None):
+    # take just the first four nearest neighbour
+    q = 0
+    for i in prange(3):
+        for j in prange(i+1,4):
+            dot_prod = np.dot(nn_matrixdiff[i+1], nn_matrixdiff[j+1])
+            i_norm = np.linalg.norm(nn_matrixdiff[i+1])
+            j_norm = np.linalg.norm(nn_matrixdiff[j+1])
+            cosphi = dot_prod/i_norm/j_norm
+            q += (cosphi + 1/3)**2
+    q = 1 - 3/8*q
+    return q
 
-        # compute distances with respect to iatom
-        if not itype:
-            dist = self.coord - self.coord[iatom]
-            dist = self.apply_pbc(dist)
-        else:
-            ss_list = []
-            for jatom, ntype in enumerate(self.type_list):
-                if ntype == itype:
-                    ss_list.append(jatom)
-            dist = self.coord[ss_list] - self.coord[iatom]
-            dist = self.apply_pbc(dist)
-        # array with the distances of the neighbours in order and list of neighbours in order
-        self.nn_dist, self.nn_list = self.compute_nn(dist)
-        # array with all the relative positions of the neighbours with respect to atom iatom
-        self.nn_matrixdiff = dist[self.nn_list]
+@jit(nopython=True)
+def apply_pbc(box, dist):
+    box = np.tile(box,(np.shape(dist)[0],1))
+    dist -= np.rint(dist/box) * box
+    return dist
 
-    def compute_q(self, iatom, itype = None):
+@jit(nopython=True)
+def compute_nn(dist):
+    dist = np.linalg.norm(dist, axis = 1)
+    ord = dist.argsort()
+    return dist[ord[:]], ord[:]
 
-        self.compute_descr(iatom, itype)
-
-        # take just the first four nearest neighbour
-        q = 0
-        for i in range(3):
-            for j in range(i+1,4):
-                dot_prod = np.dot(self.nn_matrixdiff[i+1], self.nn_matrixdiff[j+1])
-                i_norm = np.linalg.norm(self.nn_matrixdiff[i+1])
-                j_norm = np.linalg.norm(self.nn_matrixdiff[j+1])
-                cosphi = dot_prod/i_norm/j_norm
-                q += (cosphi + 1/3)**2
-        q = 1 - 3/8*q
-        return q
-
-    def apply_pbc(self, dist):
-        box = np.tile(self.box,(np.shape(dist)[0],1))
-        dist -= np.rint(dist/box) * box
-        return dist
-
-    def compute_nn(self, dist):
-        dist = np.linalg.norm(dist, axis = 1)
-        ord = dist.argsort()
-        return dist[ord[:]], ord[:]
+#class Descriptor:
+#
+#    def __init__(self, coord, box, type_list = None):
+#        self.coord = coord
+#        self.box = box
+#        self.type_list = type_list
+#
+#    def compute_descr(self, iatom, itype = None):
+#
+#        # compute distances with respect to iatom
+#        if not itype:
+#            dist = self.coord - self.coord[iatom]
+#            dist = self.apply_pbc(dist)
+#        else:
+#            ss_list = []
+#            for jatom, ntype in enumerate(self.type_list):
+#                if ntype == itype:
+#                    ss_list.append(jatom)
+#            dist = self.coord[ss_list] - self.coord[iatom]
+#            dist = self.apply_pbc(dist)
+#        # array with the distances of the neighbours in order and list of neighbours in order
+#        self.nn_dist, self.nn_list = self.compute_nn(dist)
+#        # array with all the relative positions of the neighbours with respect to atom iatom
+#        self.nn_matrixdiff = dist[self.nn_list]
+#
+#    def compute_q(self, iatom, itype = None):
+#
+#        self.compute_descr(iatom, itype)
+#
+#        # take just the first four nearest neighbour
+#        q = 0
+#        for i in range(3):
+#            for j in range(i+1,4):
+#                dot_prod = np.dot(self.nn_matrixdiff[i+1], self.nn_matrixdiff[j+1])
+#                i_norm = np.linalg.norm(self.nn_matrixdiff[i+1])
+#                j_norm = np.linalg.norm(self.nn_matrixdiff[j+1])
+#                cosphi = dot_prod/i_norm/j_norm
+#                q += (cosphi + 1/3)**2
+#        q = 1 - 3/8*q
+#        return q
+#
+#    def apply_pbc(self, dist):
+#        box = np.tile(self.box,(np.shape(dist)[0],1))
+#        dist -= np.rint(dist/box) * box
+#        return dist
+#
+#    def compute_nn(self, dist):
+#        dist = np.linalg.norm(dist, axis = 1)
+#        ord = dist.argsort()
+#        return dist[ord[:]], ord[:]
 
 
 def t_orderparameter(x, gdr, natoms, xi_c, vol):
